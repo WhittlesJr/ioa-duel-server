523a785ffe3b3a5566a70a3ca73b86014c44e7cc
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-08-01 07:51:28 -0400

    Basic hello world project implementation

---
+++ src/ioa_duel_server/core.clj
@@ -0,0 +1,6 @@
+(ns ioa-duel-server.core)
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
---
+++ test/ioa_duel_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns ioa-duel-server.core-test
+  (:require [clojure.test :refer :all]
+            [ioa-duel-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
